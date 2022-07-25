bcd9ea63ed5bfa5acbce08916b9f68e6b66e3e8a
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-07-25 21:04:30 +0530

    2nd view makin

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 let buttnDiv = document.createElement('div');
 let buttn = document.createElement('button');
 let bodyDiv = document.querySelector(".body");
+let container = document.getElementById('container');
+
+//function for event listener
+function startGame(){
+    container.style.display = "none";
+    secondVeiw();
+}
+
+function secondVeiw(){
+    
+}
+
+// button creation
 buttn.setAttribute('id','statBtn');
 buttn.textContent = "Start Game";
 buttnDiv.appendChild(buttn);
-bodyDiv.appendChild(buttnDiv)
+bodyDiv.appendChild(buttnDiv);
+
+//event listener of join button
+buttn.addEventListener("click",startGame);
