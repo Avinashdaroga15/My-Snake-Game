faa7eafe62921f36b69d2d70436ee6e68fcce61c
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-07-26 17:45:15 +0530

    adding cactus

--- game.js
+++ game.js
@@ -4,12 +4,15 @@ const foodSound = new Audio('food.mp3');
 const gameOverSound = new Audio('gameover.mp3');
 const moveSound = new Audio('move.mp3');
 const musicSound = new Audio('music.mp3');
-let speed = 5;
+const board = document.getElementById("board");
+let speed = 2;
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [{x: 15, y: 15}];
 let stone = {x: 10, y: 6};
 let food = {x: 6, y: 7};
+let cactusPos1 = {x:8,y:15}
+let cactusPos2= {x:15,y:8}
 let previousEventKey =" ";
 
 // Game Functions
@@ -23,7 +26,6 @@ function main(currentTime){
 }
 
 function gameEngine(){
-    console.log("x ",snakeArr[0].x," Y ", snakeArr[0].y)
     // Part 1: Updating the snake array & Food
     if(isCollide(snakeArr)){
         gameOverSound.play();
@@ -38,7 +40,7 @@ function gameEngine(){
     // If you have eaten the food, increment the score and regenerate the food
     if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
         foodSound.play();
-        // speed++
+        speed++;
         score += 1;
    
         if(score>highscoreval){
@@ -51,31 +53,22 @@ function gameEngine(){
         let a = 2;
         let b = 20;
         food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        stone = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        a = 2;
+        b = 20;
+        cactusPos1 = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
     }
-    else if
-    (snakeArr[0].y === stone.y && snakeArr[0].x ===stone.x){
+    else if(snakeArr[0].y === stone.y && snakeArr[0].x ===stone.x){
           
         snakeArr.pop();
         let a = 2;
         let b = 20;
+        speed--;
+        score--;
+        scoreBox.innerHTML = "Score: " + score;
         stone = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        
     }
-
-function isCollide(snake) {
-    // If you bump into yourself 
-    for (let i = 1; i < snakeArr.length; i++) {
-        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
-            return true;
-        }
-    }
-    // If you bump into the wall
-    if(snake[0].x >= 21 || snake[0].x <= 0 || snake[0].y >= 21 || snake[0].y <=0){
-        return true;
-    }
-
-     return false;
-    }
-
     // Moving the snake
     for (let i = snakeArr.length - 2; i>=0; i--) { 
         snakeArr[i+1] = {...snakeArr[i]};
@@ -114,7 +107,51 @@ function isCollide(snake) {
     stoneElement.classList.add('stone')
     board.appendChild(stoneElement);
    
+    // Display cactus 
+    let cactusDiv1 = document.createElement('div');
+    let cactus1 = document.createElement('img');
+    cactus1.setAttribute('src','cactus.png');
+    cactus1.style.maxWidth = "100%";
+    cactus1.style.maxHeight = "100%";
+    cactusDiv1.style.gridRowStart = cactusPos1.y;
+    cactusDiv1.style.gridColumnStart = cactusPos1.x;
+    cactusDiv1.appendChild(cactus1);
+    board.appendChild(cactusDiv1);    
+
+    let cactusDiv2 = document.createElement('div');
+    let cactus2 = document.createElement('img');
+    cactus2.setAttribute('src','cactus.png');
+    cactus2.style.maxWidth = "100%";
+    cactus2.style.maxHeight = "100%";
+    cactusDiv2.style.gridRowStart = cactusPos2.y;
+    cactusDiv2.style.gridColumnStart = cactusPos2.x;
+    cactusDiv2.appendChild(cactus2);
+    board.appendChild(cactusDiv2);   
 }
+function isCollide(snake) {
+    // If you bump into yourself 
+    for (let i = 1; i < snakeArr.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        }
+    }
+    // If you bump into the wall
+    if(snake[0].x >= 21 || snake[0].x <= 0 || snake[0].y >= 21 || snake[0].y <=0){
+        return true;
+    }
+
+    if(snakeArr[0].y === cactusPos1.y && snakeArr[0].x ===cactusPos1.x||snakeArr[0].y === cactusPos2.y && snakeArr[0].x ===cactusPos2.x){
+        return true;
+    }
+     return false;
+}
+
+function randomPosOfCactus(){
+    let a = 2;
+    let b = 20;
+    cactusPos2 = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+}
+
 
 // Main logic starts here
 // musicSound.play();
@@ -127,7 +164,6 @@ else{
     highscoreval = JSON.parse(highscore);
     highscoreBox.innerHTML = "HighScore: " + highscore;
 }
-
 
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
@@ -191,6 +227,9 @@ window.addEventListener('keydown', e =>{
     }
 
 });
+//random positioning of cactus
+setInterval(randomPosOfCactus,5000)
+
 // function fun(){  
 //     document.getElementById("reset").reset();  
 //   }   
