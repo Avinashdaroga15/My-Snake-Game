7c0c8e2a7c30bab4e98e2f080e42da4d12a81ef2
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-07-27 15:12:26 +0530

    link index and game files

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 let buttnDiv = document.createElement('div');
 let buttn = document.createElement('button');
 let bodyDiv = document.querySelector(".body");
-let container = document.getElementById('container-1');
-
-//function for event listener
-function startGame(){
-    container.style.display = "none";
-    secondView();
-}
-
-function secondView(){
-    
-}
+let ancher = document.createElement('a');
 
 // button creation
 buttn.setAttribute('id','statBtn');
 buttn.textContent = "Start Game";
-buttnDiv.appendChild(buttn);
+ancher.setAttribute('href','game.html');
+ancher.appendChild(buttn)
+buttnDiv.appendChild(ancher);
 bodyDiv.appendChild(buttnDiv);
-
-//event listener of join button
-buttn.addEventListener("click",startGame);
